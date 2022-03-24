4320a32937f4f3c78f63fe051de86697ed5a2979
JinkalaAnitha <101356849+JinkalaAnitha@users.noreply.github.com>
2022-03-24 10:17:17 +0530

    Update Bus_Reservation_System.c

--- 3_Implementation/Bus Reserevation.c/Bus_Reservation_System.c
+++ 3_Implementation/Bus Reserevation.c/Bus_Reservation_System.c
@@ -7,13 +7,13 @@ char name[32][100]={'\0'};
 char number[32][2]={'\0'};
 int num1[32]={0};
 int trno;
-void bus();//for list of bus
+void bus();
 void name_number(int booking,char numstr[100]);
 void booking();//for booking the tickets
-int read_number(int trno);//for reading the number from the file
-void read_name(int trno);//for reading the name from the file
-void status();//for printing the status by user input
-void status_1(int trno);//for printing the status while booking ticket
+int read_number(int trno);
+void read_name(int trno);
+void status();
+void status_1(int trno);
 void cancle();
 
 int main()
