a0bae0c1f041407389a5bf61e7c9489c469d6f96
JinkalaAnitha <101356849+JinkalaAnitha@users.noreply.github.com>
2022-03-29 22:23:45 +0530

    Add files via upload

---
+++ 3_Implementation/Documentation/function.h.h
@@ -0,0 +1,9 @@
+void bus();
+void name_number(int booking,char numstr[100]);
+void booking();
+int read_number(int trno);
+void read_name(int trno);
+void status();
+void status_1(int trno);
+void cancle();
+void login();
